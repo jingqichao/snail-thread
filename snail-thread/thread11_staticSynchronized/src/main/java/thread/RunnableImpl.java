b0b61fcf78556479e351fe4b2eb90937d78a191a
荆启超 <1209446716@qq.com>
2020-09-04 00:22:37 +0800

    使用synchronized同步方法解决线程安全问题

---
+++ snail-thread/thread11_staticSynchronized/src/main/java/thread/RunnableImpl.java
@@ -0,0 +1,28 @@
+package thread;
+
+public class RunnableImpl implements Runnable {
+
+    private static int ticket = 100;
+
+    //Object object = new Object();
+    public void run() {
+        while (true){
+            if(ticket>0){
+                payTicket();
+            }
+        }
+    }
+    //静态的同步方法解决线程安全问题，这里的锁对象是class
+    public static /**synchronized*/ void payTicket(){
+        //System.out.println(RunnableImpl.class);
+        synchronized (RunnableImpl.class){
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName()+"正在卖第"+ticket+"张票");
+            ticket--;
+        }
+    }
+}
