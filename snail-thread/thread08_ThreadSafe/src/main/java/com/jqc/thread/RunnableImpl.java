990d19d9b0e746ebb829e9960929e23597ed8a6b
荆启超 <1209446716@qq.com>
2020-09-03 22:35:30 +0800

    加sleep为了线程安全问题更好的演示

--- snail-thread/thread08_ThreadSafe/src/main/java/com/jqc/thread/RunnableImpl.java
+++ snail-thread/thread08_ThreadSafe/src/main/java/com/jqc/thread/RunnableImpl.java
@@ -6,8 +6,13 @@ public class RunnableImpl implements Runnable {
     public void run() {
         while (true){
             if(ticket>0){
-                ticket--;
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println(Thread.currentThread().getName()+"正在卖第"+ticket+"张票");
+                ticket--;
             }
         }
 
