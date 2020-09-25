0001145e543048c45def920f38bf3b40de31e696
荆启超 <1209446716@qq.com>
2020-09-25 19:38:10 +0800

    验证volatile不保证原子性，及其使用原子类AtomicInteger的getAndIncrement解决原子性

--- snail-thread/thread18_Volatile/src/main/java/LookTest.java
+++ snail-thread/thread18_Volatile/src/main/java/LookTest.java
@@ -1,16 +1,31 @@
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 class MyData{
     volatile int number = 0;
     public void add(){
         this.number = 60;
     }
-
-}
-public class LookTest {
-    //验证volatile的可见性
-    public static void main(String[] args) {
-        seeOkByVolatile();
+    public void addPlus(){
+        number++;
     }
 
+    AtomicInteger atomicInteger = new AtomicInteger();
+
+    public void addAtomicInteger(){
+        atomicInteger.getAndIncrement();
+    }
+}
+public class LookTest {
+    public static void main(String[] args) {
+
+        noAtomicity();
+    }
+
+
+
+    //验证volatile的可见性，及时通知其他线程，主物理内存的值已经被修改。
     public static void seeOkByVolatile(){
         MyData myData = new MyData();
         //新起线程
@@ -33,5 +48,27 @@ public class LookTest {
         //主线程
         System.out.println(Thread.currentThread().getName()+":"+myData.number);
     }
+    //验证volatile不保证原子性，及其使用原子类AtomicInteger的getAndIncrement解决原子性
+    public static void noAtomicity() {
+        MyData myData = new MyData();
+        //seeOkByVolatile();
+        for (int i = 0; i < 20 ; i++) {
+            new Thread(() ->{
+                for (int j = 0; j <1000 ; j++) {
+                    myData.addPlus();
+                    myData.addAtomicInteger();
+                }
+            },String.valueOf(i)).start();
+        }
+
+        while (Thread.activeCount()>2){
+            //使当前线程由执行状态，变成为就绪状态，让出cpu时间，在下一个线程执行时候，此线程有可能被执行，也有可能没有被执行。
+            Thread.yield();
+        }
+
+        System.out.println(Thread.currentThread().getName()+"\t int计算结果："+myData.number);
+        System.out.println(Thread.currentThread().getName()+"\t atomicInteger计算结果："+myData.atomicInteger);
+    }
+
 }
 
