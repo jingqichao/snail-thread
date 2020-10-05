1e12245152e3e2eef6e212fea591982058f2e8b0
荆启超 <1209446716@qq.com>
2020-10-05 15:09:19 +0800

    DCL（Double Check Lock双端检锁机制）

--- snail-thread/thread18_Volatile/src/main/java/SingletonDemo.java
+++ snail-thread/thread18_Volatile/src/main/java/SingletonDemo.java
@@ -6,9 +6,15 @@ public class SingletonDemo {
         System.out.println(Thread.currentThread().getName()+"构造方法");
     }
 
+    //DCL（Double Check Lock双端检锁机制）
     public static SingletonDemo getInstance(){
         if (instance == null){
-            instance = new SingletonDemo();
+            //在方法内部加同步代码块
+            synchronized (SingletonDemo.class){
+                if (instance==null){
+                    instance = new SingletonDemo();
+                }
+            }
         }
         return instance;
     }
